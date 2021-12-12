616bedf5b0e4b44acfd0f534b2e2465b7cc329b6
taiga07 <taigatani@me.com>
2021-12-12 03:47:42 +0000

    [add]コメント機能ルーティングまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :images, dependent: :destroy
-  
-  
-  
+  has_many :comments, dependent: :destroy
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to:'homes#top'
-  resources:images
 
+  resources:images do
+    resources:comments,only:[:create, :destroy]
+  end
 
 
   end
